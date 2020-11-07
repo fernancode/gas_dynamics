3139f0de9d357eddb1402247b2f92ad1244b4a63
Fernando <FernandoAdelaFuente@gmail.com>
2020-11-07 15:57:18 -0800

    changed readthedocs.io color theme

--- docs/conf.py
+++ docs/conf.py
@@ -17,15 +17,6 @@
 
 # -- Project information -----------------------------------------------------
 
-import sphinx_rtd_theme
-
-extensions = [
-    ...
-    "sphinx_rtd_theme",
-]
-
-html_theme = "sphinx_rtd_theme"
-
 project = 'gas_dynamics'
 copyright = '2020, Fernando de la Fuente'
 author = 'Fernando de la Fuente'
@@ -39,8 +30,9 @@ release = '0.2.4'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+import sphinx_rtd_theme
+
+extensions = ["sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -55,8 +47,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-html_theme = 'alabaster'
+
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
