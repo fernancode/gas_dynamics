8ee2428fcbdb861117f1006597577356bdce188f
Fernando <FernandoAdelaFuente@gmail.com>
2021-01-15 13:45:42 -0800

    :construction_worker: batch script for package build and deployment :rocket:

--- upload.bat
+++ upload.bat
@@ -1,11 +1,11 @@
+::to use, run "" .\upload.bat d### "" in the terminal, where d### is the distribution
+::automatically creates wheel and tar, uploads to pypi and anaconda. 
 @ECHO OFF
-::to use, run "" .\upload.bat d### "" in the terminal
 
+
+
+:: set argument equal to variable, create the path to distribution, create the wheel and tar
 set dist_folder=%1
-:: set dist = first input from command line
-
-
-:: CREATE THE WHEEL AND TAR.GZ
 set "distpath=dist/%dist_folder%"
 py setup.py sdist bdist_wheel --dist-dir %distpath%
 
@@ -15,11 +15,9 @@ set "distpath=%distpath%/*"
 py -m twine upload --repository pypi %distpath%
 
 
-:: UPLOAD TO PYPI
-:: STRING HANDLING AND FILENAME CREATION
+:: STRING HANDLING AND FILENAME CREATION FOR ANACONDA UPLOAD
 set dist=%dist_folder:~1%
 ::this makes d### -> ###
-:: CREATE THE FILENAME FOR THE CONDA UPLOAD SOME STRING HANDLING
 set first=%dist:~0,1%
 set second=%dist:~1,1%
 set third=%dist:~2,2%
@@ -28,11 +26,10 @@ IF %letter% == a set "third=%third%0"
 IF %letter% == b set "third=%third%0"
 IF %letter% == c set "third=%third%0"
 set "conda_dist=gas_dynamics-%first%.%second%.%third%.tar.gz"
-C:
+
+
 :: ACTIVATE CONDA AND UPLOAD TO CONDA
-chdir C:\ProgramData\Microsoft\Windows\"Start Menu"\Programs\"Anaconda3 (64-bit)"\
-::call "Anaconda Prompt (Anaconda3).lnk"
-::call D:
-::call chdir D:\Documents\Python\gas_dynamics\
-::call anaconda upload dist/conda_dist
-::call deactivate
+chdir dist
+call C:\ProgramData\Anaconda3\Scripts\activate.bat
+call anaconda upload %conda_dist%
+call conda deactivate
